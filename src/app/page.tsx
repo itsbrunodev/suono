8a2511c8ff46782692d72410bfd36392d1dc103a
brunolepis <brunka215@gmail.com>
2024-02-24 16:13:28 +0100

    fix: resolve suspense error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
+
 import { CountryList } from "@/components/country/list";
 import { CountrySearch } from "@/components/country/search";
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-8 p-8">
-      <CountrySearch />
-      <CountryList />
+      <Suspense fallback="Loading...">
+        <CountrySearch />
+        <CountryList />
+      </Suspense>
     </div>
   );
 }
